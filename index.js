ce22c341ad2512a5ffce54685724a42390b2d26e
bensultan1985 <bensultan1985@gmail.com>
2020-03-03 10:36:06 -0800

    second attempt

--- index.js
+++ index.js
@@ -32,8 +32,6 @@ const express = require('express');
 
 const app = express()
 
-const PORT = process.env.PORT || 5000;
-
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
